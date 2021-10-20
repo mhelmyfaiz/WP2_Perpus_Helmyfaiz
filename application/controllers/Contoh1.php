4afc1a80fa502a54463b20f09781a12cbf35b866
mhelmyfaiz <mochfhelmy@gmail.com>
2021-10-20 22:39:50 +0700

    Pertemuan 6

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Mochamad Helmy Faiz <br> Saya tinggal di daerah Tanjung priuk <br> olah raga yang saya sukai adalah Bulutangkis dan Bolabasket";
+    }
+}
