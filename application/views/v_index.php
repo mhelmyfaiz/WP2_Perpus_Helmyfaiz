3eb7b8b84ae51094cefd263f810fef946dbbcaa5
mhelmyfaiz <90606019+mhelmyfaiz@users.noreply.github.com>
2021-11-16 01:52:27 +0700

    pertemuan 4

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -13,7 +13,7 @@
         <li>Controller</li>
         <p align="justify">Controller merupakan kumpulan intruksi aksi yang menghubungkan model dan view, jadi user tidak akan berhubungan dengan model secara langsung, intinya data yang tersimpan di database (model) di ambil oleh controller dan kemudian controller pula yang menamenampilkan nya ke view. Jadi controller lah yang mengolah intruksi.</p>
         <p align="justify">Dari penjelasan tentang model view dan controller di atas dapat di simpulkan bahwa controller sebagai penghubung view dan model. Misalnya pada aplikasi yang menampilkan data dengan menggunakan metode konsep mvc, controller memanggil intruksi pada model yang mengambil data pada database, kemudian controller yang meneruskannya pada view untuk di tampilkan. Jadi jelas sudah dan sangat mudah dalam pengembangan aplikasi dengan cara mvc ini karena web designer atau front-end developer tidak perlu lagi berhubungan dengan controller, dia hanya perlu berhubungan dengan view untuk mendesign tampilann aplikasi, karena back-end developer yang menangani bagian controller dan modelnya. Jadi pembagian tugas pun menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan cepat dan terstruktur.</p>
-    </section>
+		</section>
 
 
 
