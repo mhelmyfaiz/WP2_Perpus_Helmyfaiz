17b1d1bde2d21b28e3ec6cdddd97a2165b237bf4
mhelmyfaiz <90606019+mhelmyfaiz@users.noreply.github.com>
2021-11-16 01:51:47 +0700

    pertemuan 4

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Web Pro II | Merancang Template sederhana dengan codeigniter</title>
+	<title>Web Pro II | Merancang Template sederhana dengan codeigniter</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
 </head>
 <body>
