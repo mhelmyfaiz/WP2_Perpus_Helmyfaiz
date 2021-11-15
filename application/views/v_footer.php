9905efce7894f26da8926938a00a67896c5b6ac4
mhelmyfaiz <90606019+mhelmyfaiz@users.noreply.github.com>
2021-11-16 01:51:20 +0700

    pertemuan 4

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -1,5 +1,5 @@
         <footer> 
-            <a href="http://www.RentalBuku.com">RentalBuku</a> 
+            <a href="http://www.RentalBuku.com">RentalBukui</a> 
          </footer> 
     </div> 
 </body> 
